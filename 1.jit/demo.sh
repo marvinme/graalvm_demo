64c450e97ccddc74c59d864d7a0bada530f794b6
marvin <marvin.kim.hi@gmail.com>
2020-11-18 12:52:10 +0900

    update

--- 1.jit/demo.sh
+++ 1.jit/demo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+java -version
 javac CountUppercase.java
 echo -e "\033[36m"==================== Hotspot ===================="\033[0m"
 time java -XX:-UseJVMCICompiler CountUppercase Welcome to Oracle Developer World!
