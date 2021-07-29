fa5b5f61b57b1868009c27e7cb33dca9b62c67b1
marvin <marvin.kim.hi@gmail.com>
2021-07-29 14:39:48 +0900

    update

--- 1.jit/demo.sh
+++ 1.jit/demo.sh
@@ -5,5 +5,9 @@ echo -e "\033[36m"==================== Hotspot ===================="\033[0m"
 time java -XX:-UseJVMCICompiler CountUppercase Welcome to Oracle Developer World!
 read
 
+echo -e "\033[33m"==================== Graal CE ===================="\033[0m"
+time java -Dgraal.ShowConfiguration=info -Dgraal.CompilerConfiguration=community -Dgraal.ShowConfiguration=info CountUppercase Welcome to Oracle Developer World!
+read
+
 echo -e "\033[31m"==================== Graal EE ===================="\033[0m"
 time java -Dgraal.ShowConfiguration=info CountUppercase Welcome to Oracle Developer World!
--- 1.jit/jvms_compare.sh
+++ 1.jit/jvms_compare.sh
@@ -5,5 +5,6 @@ for i do
 #  printf '%s\n'   "$i"
    sdk use java $i > /dev/null 2>&1
    result=$(java CountUppercase Welcome to Oracle Developer World! | grep total)
-   echo -e $i'\t': ${result:17:7}
+   number=$(echo ${result:17:7} | tr -cd [0-9] )
+   echo -e $i'\t': $number ms
 done
