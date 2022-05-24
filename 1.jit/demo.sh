11e8ec21041ba0485712b5709dde1f72c38fb3b5
marvinme <50611452+marvinme@users.noreply.github.com>
2022-05-24 16:40:27 +0900

    Update demo.sh

--- 1.jit/demo.sh
+++ 1.jit/demo.sh
@@ -3,11 +3,11 @@ java -version
 javac CountUppercase.java
 echo -e "\033[36m"==================== Hotspot ===================="\033[0m"
 time java -XX:-UseJVMCICompiler CountUppercase Welcome to Oracle Developer World!
-read
+#read
 
 echo -e "\033[33m"==================== Graal CE ===================="\033[0m"
 time java -Dgraal.ShowConfiguration=info -Dgraal.CompilerConfiguration=community -Dgraal.ShowConfiguration=info CountUppercase Welcome to Oracle Developer World!
-read
+#read
 
 echo -e "\033[31m"==================== Graal EE ===================="\033[0m"
 time java -Dgraal.ShowConfiguration=info CountUppercase Welcome to Oracle Developer World!
